48555016c16e3b31ae17879aabba78f1521ba0d4
Samuel Opoku Asare <asare11samuel@gmail.com>
2021-03-11 02:58:57 +0000

    Added event listeners to buttons

--- app/src/main/java/com/asvtech/buttontry/MainActivity.kt
+++ app/src/main/java/com/asvtech/buttontry/MainActivity.kt
@@ -3,6 +3,7 @@ package com.asvtech.buttontry
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import com.asvtech.buttontry.databinding.ActivityAlertDialoggBinding
 import com.asvtech.buttontry.databinding.ActivityMainBinding
@@ -28,6 +29,69 @@ class MainActivity : AppCompatActivity() {
         binding.btnDatePicker.setOnClickListener {
 
             val intent = Intent(this, DatePickerr::class.java)
+            startActivity(intent)
+
+        }
+
+        binding.btnAutoComplete.setOnClickListener {
+
+            val intent = Intent(this, AutoCompleteTextvieww::class.java)
+            startActivity(intent)
+
+        }
+
+        binding.btnCheckBox.setOnClickListener {
+
+            val intent = Intent(this, ChkBox::class.java)
+            startActivity(intent)
+
+        }
+
+        binding.btnImageButton.setOnClickListener {
+
+            val intent = Intent(this, ImageeButton::class.java)
+            startActivity(intent)
+
+        }
+
+        binding.btnProgressBar.setOnClickListener {
+
+            val intent = Intent(this, ProgressBarr::class.java)
+            startActivity(intent)
+
+        }
+
+        binding.btnRatingBar.setOnClickListener {
+
+            val intent = Intent(this, RatingBarr::class.java)
+            startActivity(intent)
+
+        }
+
+        binding.btnSpinner.setOnClickListener {
+
+            val intent = Intent(this, spinner_example::class.java)
+            startActivity(intent)
+
+        }
+
+        binding.btnSwitchButton.setOnClickListener {
+
+            val intent = Intent(this, SwitchButton::class.java)
+            startActivity(intent)
+
+        }
+
+        binding.btnToast.setOnClickListener {
+
+            val intent = Intent(this, Toast::class.java)
+            startActivity(intent)
+
+        }
+
+        binding.btnToggleButton.setOnClickListener {
+
+            val intent = Intent(this, ToggleButton::class.java)
             startActivity(intent)
 
         }
